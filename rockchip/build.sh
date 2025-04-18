241e34e730e6c358647033f023a2cde6d3fa2db2
yizhonghe98 <63568796+yizhonghe98@users.noreply.github.com>
2025-04-18 18:26:11 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -52,7 +52,7 @@ PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
